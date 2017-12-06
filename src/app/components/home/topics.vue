e92c1bf7f17985058fefc10eed31fcdc220269f7
shinhwazt <m13833889355@163.com>
2017-12-06 18:00:51 +0800

    update

--- src/app/components/home/topics.vue
+++ src/app/components/home/topics.vue
@@ -39,6 +39,9 @@ export default{
     },function(){
 
     });
+
+
+
   }
 
 }
--- src/app/store/action.js
+++ src/app/store/action.js
@@ -1,6 +1,5 @@
 export default{
   ["ACTIONS_HANDLER_TEST"]({commit,state},obj){
-    
     setTimeout(function(){
       commit("HANDLER_TEST",obj);
     },1000)
